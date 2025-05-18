a258509ef463e4d144d599528e4d66254dfa525a
Aanchal Acharya <144967783+aanch013@users.noreply.github.com>
2025-05-18 12:54:44 -0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,1 +1,21 @@
 # task 5 - what are the most significant features (e.g., tempo, energy, danceability) that predict a song’s popularity?
+from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml import Pipeline
+
+df = spark.read.csv("updated_cleaned_dataset.csv", header=True, inferSchema=Tru>
+df.selectfrom pyspark.sql import SparkSession
+
+features = [
+    "Genre", "Tempo", "Explicit", "Energy", "Danceability",
+    "Positiveness", "Liveness", "Good for Party", "Good for Social Gatherings"
+]
+
+assembler = VectorAssembler(inputCols=features, outputCol="features_unscaled")
+scaler = StandardScaler(inputCol="features_unscaled", outputCol="features", wit>
+
+lr = LinearRegression(featuresCol="features", labelCol="popularity")
+
+pipeline = Pipeline(stages=[assembler, scaler, lr])
+model = pipeline.fit(df_clean)
+lr_model = model.stages[-1]
