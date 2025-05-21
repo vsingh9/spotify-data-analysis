d271f28bde80b3cd605bc76aa7e5d692c3df4b5c
Aanchal Acharya <144967783+aanch013@users.noreply.github.com>
2025-05-20 20:18:22 -0700

    Added task 5 to spotify.py

--- spotify.py
+++ spotify.py
@@ -1,8 +1,10 @@
 import time
-from pyspark.sql.functions import split, explode, trim, col, year, to_date, count, avg, round, countDistinct
+from pyspark.sql.functions import split, explode, trim, col, year, to_date, count, avg, round, countDistinct, corr
 from pyspark.sql import SparkSession
 import pandas as pd
 import logging
+import matplotlib.pyplot as plt
+import time
 logger = logging.getLogger('py4j')
 logger.setLevel(logging.ERROR)
 
@@ -50,3 +52,58 @@ yearly_counts.coalesce(1).write.mode("overwrite").option("header", "true").csv("
 # ---------- Total Time ----------
 print(f"\nTotal script runtime: {time.time() - start_time:.2f} seconds")
 # Task 3 end
+
+
+#Task 5 start
+
+spark = SparkSession.builder \
+    .appName("Spotify Correlation Task 5") \
+    .getOrCreate()
+
+start_time_task5 = time.time()
+spark.sparkContext.setLogLevel("WARN")
+spark.conf.set("spark.sql.debug.maxToStringFields", 9)
+
+df = spark.read.csv("updated_cleaned_dataset.csv", header=True, inferSchema=True)
+
+from pyspark.ml.feature import StringIndexer
+genre_indexer = StringIndexer(inputCol="Genre", outputCol="Genre_index", handleInvalid="skip")
+explicit_indexer = StringIndexer(inputCol="Explicit", outputCol="Explicit_index", handleInvalid="skip")
+
+df = genre_indexer.fit(df).transform(df)
+df = explicit_indexer.fit(df).transform(df)
+
+features = [
+    "Genre_index", "Tempo", "Explicit_index", "Energy", "Danceability",
+    "Positiveness", "Liveness", "Good for Party", "Good for Social Gatherings"
+]
+
+correlations = []
+print("\nFeature Correlations (Pearson r with Popularity):")
+for feat in features:
+    cor_val = df.stat.corr(feat, "Popularity")
+    correlations.append((feat, cor_val))
+    print(f"{feat:25}: {cor_val:.4f}")
+
+for feat in features:
+    cor_val = df.stat.corr(feat, "Popularity")
+    correlations.append((feat, cor_val))
+
+coef_data = pd.DataFrame(correlations, columns=["Feature", "Coefficient"])
+coef_data["AbsCoeff"] = coef_data["Coefficient"].abs()
+coef_data.sort_values("AbsCoeff", ascending=True, inplace=True)
+
+coef_data.to_csv("feature_importance_correlation.csv", index=False)
+
+plt.figure(figsize=(10, 6))
+plt.barh(coef_data["Feature"], coef_data["Coefficient"], color="steelblue")
+plt.axvline(0, color="gray", linestyle="--")
+plt.xlabel("Pearson Correlation with Popularity")
+plt.title("Feature Correlation with Song Popularity")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("feature_correlation.png")
+plt.show()
+
+print(f"\nTotal script runtime: {time.time() - start_time_task5:.2f} seconds")
+#Task 5 end
